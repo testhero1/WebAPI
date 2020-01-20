8ae1b4cde3cf077ed35a785712e8e781c6456ba8
testhero1 <testhero1@users.noreply.github.com>
2020-01-19 21:05:17 -0500

    initial setup

---
+++ WebApplication1/Controllers/ExpenseController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using WebApplication1.Models;
+using System.IO;
+using System.Text;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpenseController : ControllerBase
+    {
+
+
+        private readonly ILogger<ExpenseController> _logger;
+
+        public ExpenseController(ILogger<ExpenseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            var items = new[]
+                {
+                    new {id = "1" , name = "Cable"},
+                    new {id = "2" , name= "Movies"}
+                };
+            
+            return items;
+        }
+    } 
+}
+
+
+
---
+++ WebApplication1/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Expense
+    {
+        public int id { get;  set; } 
+        public string name { get; set; }
+    }
+}
